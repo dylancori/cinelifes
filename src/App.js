5b0456e97b6593ce5cd03d63c256338927d76996
dylancori <dylancoriglian@gmail.com>
2022-11-23 19:31:01 -0300

    PreEntrega1+corigliano

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ItemListContainer from './components/NavBar/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
+import CartWidget from './components/CartWidget/CartWidget';
 function App() {
   return (
-    <NavBar />
+    <div className='app'>
+    <NavBar/>
+    <CartWidget />
+    <ItemListContainer name='dylan'/>
+    </div>
   )
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,65 @@
-import React from 'react'
-import CartWidget from '../CartWidget/CartWidget'
-const NavBar = () => {
+// import React from 'react'
+// import CartWidget from '../CartWidget/CartWidget'
+// const NavBar = () => {
+//   return (
+//     <div>
+//         <nav>
+//             <ul>
+//                 <li>home</li>
+//                 <li>terror</li>
+//                 <li>drama</li>
+//                 <li>comedia</li>
+//             </ul>
+//         </nav>
+//         {/* <CartWidget /> */}
+//     </div>
+//   )
+// }
+
+// export default NavBar
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function BasicExample() {
   return (
     <div>
-        <nav>
-            <ul>
-                <li>home</li>
-                <li>terror</li>
-                <li>drama</li>
-                <li>comedia</li>
-            </ul>
-        </nav>
-        <CartWidget />
+    <Navbar bg="light" expand="xl">
+      <Container>
+        <Navbar.Brand href="#home">CINELIFE</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            
+            <NavDropdown title="series" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">terror</NavDropdown.Item>
+              <br></br>
+              <NavDropdown.Item href="#action/3.2">drama</NavDropdown.Item>
+              <br></br>
+              <NavDropdown.Item href="#action/3.3">comedia</NavDropdown.Item>
+              
+            </NavDropdown>
+
+            <NavDropdown title="peliculas" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">terror</NavDropdown.Item>
+              <br></br>
+              <NavDropdown.Item href="#action/3.2">drama</NavDropdown.Item>
+              <br></br>
+              <NavDropdown.Item href="#action/3.3">comedia</NavDropdown.Item>
+              
+            </NavDropdown>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default BasicExample;
